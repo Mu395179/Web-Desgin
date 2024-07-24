249cd5c2420c8575ed1e0ad3303748634addb351
Mu395179 <d128579744@gmail.com>
2024-07-24 17:28:36 +0800

    新增login model功能

--- form_login/login.php
+++ form_login/login.php
@@ -9,34 +9,52 @@
 </head>
 
 <body>
-    <legend>Admin Login</legend>
-    <table>
-        <tr>
-            <td>帳號</td>
-            <td><input type="text" name="username" id="acc" value="admin"></td>
-        </tr>
-        <tr>
-            <td>密碼</td>
-            <td><input type="password" name="password" id="pw" value="1234"></td>
-        </tr>
-        <tr>
-            <td>
-                <button onclick="login()">登入</button>
-                <button onclick="clearFields()">清除</button>
-            </td>
 
-        </tr>
-    </table>
-    </fieldset>
+    <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="loginModalLabel">Modal title</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <legend>Admin Login</legend>
+                    <table>
+                        <tr>
+                            <td>帳號</td>
+                            <td><input type="text" name="username" id="acc" value="admin"></td>
+                        </tr>
+                        <tr>
+                            <td>密碼</td>
+                            <td><input type="password" name="password" id="pw" value="1234"></td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <button onclick="login()">登入</button>
+                                <button onclick="clearFields()">清除</button>
+                            </td>
+
+                        </tr>
+                    </table>
+                </div>
+               
+            </div>
+        </div>
+    </div>
+
+
+
+
+
     <script>
         function login() {
 
-            $.post('./check_acc.php', {
+            $.post('../form_login/check_acc.php', {
                 acc: $("#acc").val()
             }, (chkAcc) => {
                 console.log(chkAcc);
                 if (parseInt(chkAcc) == 1) {
-                    $.post("./check_pass.php", {
+                    $.post("../form_login/check_pass.php", {
                         acc: $("#acc").val(),
                         pw: $("#pw").val()
                     }, (chkPw) => {
@@ -56,6 +74,18 @@
             $("#acc").val('');
             $("#pw").val('');
         }
+
+
+        const loginModal = new bootstrap.Modal('#loginModal')
+        const modal = document.querySelector("#loginModal")
+        modal.addEventListener('hidden.bs.modal', event => {
+
+            loginModal.dispose()
+            $("#modal").html("")
+
+        })
+        //console.log(loginModal)
+        loginModal.show()
     </script>
 </body>
 
